52daea33f4f54ae659120614a630c521a04131fa
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2024-04-08 12:40:26 +0200

    1/setup storybook (#20)
    * Add Storybook
    
    * Initialize Storybook
    
    * Connect tailwind and storybook
    
    * Fix path to folder public
    
    * Add dummy components for Storybook testing
    
    * Add clasnames
    
    * Add tailwind-merge
    
    * Add react-aria-components
    
    * Regenerate yarn lock
    
    * Add missing eslint dependencies
    
    * Add react-aria
    
    * Fix errors in dummy components

---
+++ next/src/app/_components/storybook-test-components/BAButton/ui-icons/index.ts
@@ -0,0 +1,2 @@
+export { default as ExportIcon } from './export.svg'
+export { default as ArrowRightIcon } from './sipka-doprava.svg'
---
+++ next/src/app/_components/storybook-test-components/TestButtonAppFolder.tsx
@@ -0,0 +1,13 @@
+// type ChipProps = TagProps & {
+//   variant?: 'large' | 'small'
+// }
+
+const Chip = () => {
+  return (
+    <button type="button" className="border-4 bg-red-400">
+      Hello
+    </button>
+  )
+}
+
+export default Chip

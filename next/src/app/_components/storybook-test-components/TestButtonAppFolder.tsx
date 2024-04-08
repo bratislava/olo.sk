bcca32fa5d83ebcac57faf1576b97b3d0be3c324
Adam <jakubicka.adam@gmail.com>
2024-04-08 10:38:20 +0200

    Add dummy components for Storybook testing

---
+++ next/src/app/_components/storybook-test-components/TestButtonAppFolder.tsx
@@ -0,0 +1,15 @@
+import cx from 'classnames'
+
+// type ChipProps = TagProps & {
+//   variant?: 'large' | 'small'
+// }
+
+const Chip = () => {
+  return (
+    <button type="button" className="border-4 bg-red-400">
+      Hello
+    </button>
+  )
+}
+
+export default Chip

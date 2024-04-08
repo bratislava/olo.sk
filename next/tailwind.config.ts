28856143dfda2487941cba5d0e99892982f84fa8
Adam <jakubicka.adam@gmail.com>
2024-04-08 10:31:58 +0200

    Connect tailwind and storybook

--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -30,6 +30,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx}',
   ],
   plugins: [
     removeNativeSearchInputStyling,

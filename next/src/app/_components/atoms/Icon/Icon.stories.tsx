7d6573f05f8e44b3a1af397bba2ef9c350c0de9f
Adam <jakubicka.adam@gmail.com>
2024-04-09 13:46:52 +0200

    Configure Storybook to support SVGs and add Icon story

---
+++ next/src/app/_components/atoms/Icon/Icon.stories.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Meta, StoryObj } from '@storybook/react'
+import { twMerge } from 'tailwind-merge'
+
+import IconComponent from './Icon'
+
+const meta: Meta<typeof IconComponent> = {
+  component: IconComponent,
+  title: 'Components/Icon',
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div className="flex justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<typeof IconComponent>
+
+export const Icon: Story = {
+  args: {
+    name: 'domcek',
+    className: '',
+  },
+  render: (args) => <IconComponent {...args} className={twMerge('w-12', args.className)} />,
+}

3a9de0ec28b817b63a2549832756f623d88fcdbf
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-04-09 12:34:43 +0200

    update eslint and prettier

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -6,12 +6,12 @@ module.exports = {
     /** Too tedious to type every function return explicitly */
     '@typescript-eslint/explicit-function-return-type': 'off',
     /** We prefer arrow functions */
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
     /** It's annoying to refactor from one style to another */
     'arrow-body-style': 'off',
     /** These are exceptions that we use with "__" */
     'no-underscore-dangle': [
-      2,
+      'error',
       { allow: ['__NEXT_DATA__', '__NEXT_LOADED_PAGES__', '__typename'] },
     ],
     /** Links get confused for secrets */
@@ -23,30 +23,23 @@ module.exports = {
     /** This is no longer needed since React 17 */
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    /** Solve warning "Promise-returning function provided to attribute where a void return was expected." */
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: { attributes: false } },
+    ],
+    '@typescript-eslint/no-floating-promises': 'warn',
 
     'lodash/prefer-noop': 'off',
     'pii/no-phone-number': 'off',
     'xss/no-mixed-html': 'off',
 
-    // TODO: Turned off because of some missing setup
+    // TODO revisit
     'import/extensions': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-    // Solve warning "Promise-returning function provided to attribute where a void return was expected."
-    // '@typescript-eslint/no-misused-promises': [
-    //   2,
-    //   {
-    //     checksVoidReturn: {
-    //       attributes: false,
-    //     },
-    //   },
-    // ],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
+
+    /* Formatting rules */
+    'prettier/prettier': ['error', { endOfLine: 'auto' }], // TODO revisit, prettier should not be run by eslint
+    'padding-line-between-statements': ['warn', { blankLine: 'always', prev: '*', next: 'return' }],
   },
   ignorePatterns: ['*.config.*', '.eslintrc.js', 'src/services/graphql/index.ts'],
 }

f1d8bee1364ec6362fb9d0bfefb410b2c85eaa78
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-04-11 16:00:18 +0200

    fix healthcheck api endpoint to new app router syntax (#40)

--- next/src/app/pages/api/healthcheck.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  return res.json({ status: 'ok' })
-}
-
-export default handler

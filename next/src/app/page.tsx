a02eabb3b3e0084f25b043a6dfc9e5742295743a
Adam <jakubicka.adam@gmail.com>
2024-04-11 12:13:25 +0200

    Rename folder atoms to common

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Icon from '@/app/_components/atoms/Icon/Icon'
+import Icon from '@/app/_components/common/Icon/Icon'
 
 const Home = async () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">

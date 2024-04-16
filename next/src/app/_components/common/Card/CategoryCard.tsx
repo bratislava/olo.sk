0ad648e8fe9f1f4ed12117e99bf23781b36a831b
Adam <jakubicka.adam@gmail.com>
2024-04-16 15:04:13 +0200

    Add CategoryCard

---
+++ next/src/app/_components/common/Card/CategoryCard.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
+
+import CardBase from '@/app/_components/common/Card/CardBase'
+import CardLink from '@/app/_components/common/Card/CardLink'
+import ImagePlaceholder from '@/app/_components/common/ImagePlaceholder'
+import Typography from '@/app/_components/common/Typography/Typography'
+import { generateImageSizes } from '@/app/_utils/generateImageSizes'
+
+type CategoryCardProps = {
+  title: string
+  linkHref: string
+  linkText?: string
+  className?: string
+  imgSrc?: string
+}
+
+/*
+ * FIGMA: https://www.figma.com/file/2qF09hDT9QNcpdztVMNAY4/OLO-Web?type=design&node-id=1199-13816&mode=dev
+ */
+
+const CategoryCardTitle = ({ text }: { text: string }) => (
+  <Typography variant="h5" className_onlyWhenNecessary="line-clamp-3">
+    {text}
+  </Typography>
+)
+const CategoryCardImage = ({ imgSrc }: { imgSrc?: string }) => {
+  const imageSizes = generateImageSizes({ default: '100vw', md: '50vw', lg: '33vw' })
+
+  return (
+    <div className="relative aspect-square shrink-0 overflow-hidden rounded-lg">
+      {imgSrc ? (
+        <Image src={imgSrc} sizes={imageSizes} alt="" className="object-cover" />
+      ) : (
+        <ImagePlaceholder />
+      )}
+    </div>
+  )
+}
+
+const CategoryCard = ({ title, className, linkHref, linkText, imgSrc }: CategoryCardProps) => {
+  return (
+    <CardBase className={twMerge('gap-4 bg-white', className)}>
+      <CategoryCardImage imgSrc={imgSrc} />
+      <div className="flex flex-col gap-2 ">
+        <CategoryCardTitle text={title} />
+        <CardLink linkHref={linkHref} customText={linkText} />
+      </div>
+    </CardBase>
+  )
+}
+
+export default CategoryCard

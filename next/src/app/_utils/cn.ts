06ebd78d87867ae8338bc4df4190a40fda319f2f
Adam <jakubicka.adam@gmail.com>
2024-04-24 09:31:25 +0200

    Reorder again

--- next/src/app/_utils/cn.ts
+++ next/src/app/_utils/cn.ts
@@ -1,7 +1,7 @@
 // cn helper function inspired by https://ui.shadcn.com/docs/installation/manual
 
-import { twMerge } from 'tailwind-merge'
 import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 function cn(...args: ClassValue[]) {
   return twMerge(clsx(args))

3bc27f3db25075abd4058ef5d3e9aac199cb413f
Adam <jakubicka.adam@gmail.com>
2024-04-26 12:55:21 +0200

    Fix styles in Button

--- next/src/_components/common/Button/Button.tsx
+++ next/src/_components/common/Button/Button.tsx
@@ -12,6 +12,10 @@ import Icon from '@/_components/common/Icon/Icon'
 import Link, { LinkPlausibleProps } from '@/_components/common/Link/Link'
 import Spinner from '@/_components/common/Spinner/Spinner'
 
+/*
+ *  Figma: https://www.figma.com/file/2qF09hDT9QNcpdztVMNAY4/OLO-Web?type=design&node-id=4-385&mode=design&t=IDCmW43zimnlwiDU-4
+ */
+
 type ButtonOrIconButton =
   | {
       icon: ReactNode
@@ -28,20 +32,19 @@ type ButtonOrIconButton =
     }
 
 type ButtonBase = {
-  variant?:
+  variant:
     | 'unstyled'
     | 'icon-wrapped'
     | 'icon-wrapped-negative-margin'
-    | 'category-solid'
-    | 'category-outline'
-    | 'category-plain'
-    | 'black-solid'
-    | 'black-outline'
-    | 'black-plain'
-    | 'negative-solid'
-    | 'negative-plain'
-    | 'black-link'
-    | 'category-link'
+    | 'category-solid' // Figma: Primary
+    | 'category-outline' // Figma: Secondary
+    | 'category-plain' // Figma: Plain
+    | 'black-solid' // Figma: Primary inverted
+    | 'black-outline' // Figma: Tertiary
+    | 'black-plain' // not in Figma, but we implement it
+    | 'negative-solid' // TODO not in Figma
+    | 'negative-plain' // TODO not in Figma
+    | 'black-link' // Figma: Default link
   size?: 'responsive' | 'large' | 'small'
   className?: string
   fullWidth?: boolean
@@ -103,18 +106,20 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProp
     /* TODO
      *   - examine why `text-button` interferes with `text-[color]` and therefore is sometimes ignored
      *   - border should render inside button, not outside
-     *   - focus text color for 'culture' and 'social' category should be -800
      */
-    const styles =
+    const styles = twMerge(
+      /*
+       *  We use isFocusVisible to show focus ring only on keyboard navigation
+       *  It's recommended to remove default outline and use custom styling as ring:
+       *  https://tailwindcss.com/docs/outline-style#removing-outlines
+       */
+      'outline-none ring-offset-2 focus-visible:ring',
       variant === 'unstyled'
         ? className ?? ''
         : twMerge(
             // TODO text-button interferes with text-[color], as quickfix we set size and color here by arbitrary values
             'inline-flex h-auto items-center justify-center gap-2 text-[1rem] font-semibold leading-[1.5rem] transition',
             cx(
-              // we use isFocusVisible to show focus ring only on keyboard navigation
-              // it's recommended to remove default outline and use custom styling as ring: https://tailwindcss.com/docs/outline-style#removing-outlines
-              'outline-none ring-offset-2 focus-visible:ring',
               // we change rounded corners for link focus ring
               isLinkVariant ? 'rounded-sm max-lg:gap-1' : 'rounded-lg',
 
@@ -135,7 +140,7 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProp
                 'w-fit': !fullWidth && !fullWidthMobile,
 
                 // border width
-                'border-2': isSolidOrOutlineVariant,
+                border: isSolidOrOutlineVariant,
 
                 // padding - link variants
                 'p-0': isLinkVariant,
@@ -166,46 +171,47 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProp
                 'p-1.5': size === 'small' && isIconButton && isPlainVariant,
                 'p-2': size === 'large' && isIconButton && isPlainVariant,
 
-                // colors - bg, border, text - idle & focus
-                'border-action-background bg-action-background text-content-secondary pressed:border-action-background-pressed pressed:bg-action-background-pressed':
+                // colors: idle & focus (background, border, text)
+
+                'bg-action-background-default border-action-background-default text-content-secondary pressed:border-action-background-pressed pressed:bg-action-background-pressed':
                   variant === 'category-solid',
-                'border-action-border bg-transparent text-content-secondary pressed:border-action-border-pressed':
+                'border-action-border-default bg-transparent text-content-secondary pressed:border-action-border-pressed':
                   variant === 'category-outline',
-
-                'border-gray-700 bg-gray-700 pressed:border-gray-800 pressed:bg-gray-800 text-white':
-                  variant === 'black-solid',
-                'border-gray-200 text-gray-700 pressed:border-gray-300 pressed:text-gray-800 bg-transparent':
-                  variant === 'black-outline',
-                // 'border-gray-200 text-gray-700 pressed:border-gray-300 pressed:text-gray-800 bg-white':
-                'border-negative-700 bg-negative-700 pressed:border-negative-800 pressed:bg-negative-800 text-white':
-                  variant === 'negative-solid',
-
                 'text-content-secondary pressed:bg-action-softBackground-pressed':
                   variant === 'category-plain',
-                'text-gray-700 pressed:bg-gray-200 pressed:text-gray-800':
-                  variant === 'black-plain',
-                'text-negative-700 pressed:bg-negative-200 pressed:text-negative-800':
-                  variant === 'negative-plain',
 
-                'text-category-700 pressed:text-category-800': variant === 'category-link',
-                'text-gray-700 pressed:text-gray-800': variant === 'black-link',
+                'border-action-content-default bg-action-content-default text-content-primaryInverted pressed:border-action-content-pressed pressed:bg-action-content-pressed':
+                  variant === 'black-solid',
+                'border-border-default text-action-content-default bg-transparent pressed:border-action-content-pressed pressed:text-border-dark':
+                  variant === 'black-outline',
+                'text-action-content-default pressed:bg-border-hover': variant === 'black-plain',
 
-                // colors:hover - bg, border, text
-                'hovered:border-action-background-hover hovered:bg-action-background-hover':
+                'text-action-content-default pressed:text-gray-800': variant === 'black-link',
+
+                // 'border-border-default text-action-content-default pressed:border-border-hover pressed:text-gray-800 bg-white':
+                // 'border-negative-700 bg-negative-700 pressed:border-negative-800 pressed:bg-negative-800 text-content-primaryInverted':
+                //   variant === 'negative-solid',
+                // 'text-negative-700 pressed:bg-negative-200 pressed:text-negative-800':
+                //   variant === 'negative-plain',
+
+                // colors:hover (background, border, text)
+
+                'hover:border-action-background-hover hover:bg-action-background-hover':
                   variant === 'category-solid',
-                'hovered:border-action-border-hover hovered:bg-action-border-hover':
-                  variant === 'category-outline',
-                'hovered:bg-softBackground-hover': variant === 'category-plain',
+                'hover:border-action-border-hover': variant === 'category-outline',
+                'hover:bg-action-softBackground-hover': variant === 'category-plain',
 
-                'hover:border-gray-600 hover:bg-gray-600': variant === 'black-solid',
-                'hover:border-gray-200 hover:text-gray-600': variant === 'black-outline',
-                'hover:bg-gray-100 hover:text-gray-600': variant === 'black-plain',
+                'hover:border-action-content-hover hover:bg-action-content-hover':
+                  variant === 'black-solid',
+                'hover:border-action-content-hover hover:text-action-content-hover':
+                  variant === 'black-outline',
+                'hover:bg-border-default hover:text-action-content-default':
+                  variant === 'black-plain',
 
-                'hover:border-negative-600 hover:bg-negative-600': variant === 'negative-solid',
-                'hover:bg-negative-100 hover:text-negative-600': variant === 'negative-plain',
-
-                'hover:text-category-600': variant === 'category-link',
                 'hover:text-gray-600': variant === 'black-link',
+
+                // 'hover:border-negative-600 hover:bg-negative-600': variant === 'negative-solid',
+                // 'hover:bg-negative-100 hover:text-negative-600': variant === 'negative-plain',
 
                 // svg icons
                 '[&>svg]:h-5 [&>svg]:w-5 [&>svg]:lg:h-6 [&>svg]:lg:w-6': size === 'responsive',
@@ -214,7 +220,8 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProp
               },
             ),
             className,
-          )
+          ),
+    )
 
     if (rest.href) {
       const isExternal = rest.href.startsWith('http')

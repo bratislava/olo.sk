0d21df50f21e07cc732f2ed070908d703c15049e
Adam <jakubicka.adam@gmail.com>
2024-04-29 23:39:50 +0200

    Add MenuItemArticleCard

---
+++ next/src/_components/common/Card/MenuItemArticleCard.tsx
@@ -0,0 +1,53 @@
+import CardBase from '@/_components/common/Card/CardBase'
+import CardImage from '@/_components/common/Card/CardImage'
+import Link from '@/_components/common/Link/Link'
+import Tag from '@/_components/common/Tag/Tag'
+import Typography from '@/_components/common/Typography/Typography'
+
+type MenuItemArticleCardProps = {
+  title: string
+  linkHref: string
+  tagText: string
+  className?: string
+  imgSrc?: string
+}
+
+/**
+ * FIGMA: https://www.figma.com/design/2qF09hDT9QNcpdztVMNAY4/OLO-Web?node-id=1949-19741&m=dev
+ */
+
+const MenuItemArticleCard = ({
+  title,
+  className,
+  linkHref,
+  imgSrc,
+  tagText,
+}: MenuItemArticleCardProps) => {
+  return (
+    <CardBase className={className}>
+      <div className="flex flex-col gap-4 lg:flex-row">
+        {/* 6.25rem = 100px */}
+        <CardImage imgSrc={imgSrc} className="aspect-[100/56] w-[6.25rem] self-start rounded-lg" />
+        <div className="flex flex-col gap-3">
+          <Tag variant="without-bg" text={tagText} />
+          <Typography
+            variant="h6"
+            className_onlyWhenNecessary="line-clamp-3 group-hover/CardBase:underline"
+          >
+            {title}
+          </Typography>
+          {/* TODO Add aria-label */}
+          <Link
+            variant="unstyled"
+            href={linkHref}
+            aria-label=""
+            stretched
+            className="ml-auto self-center p-1.5 max-lg:hidden [&>svg:last-of-type]:hidden"
+          />
+        </div>
+      </div>
+    </CardBase>
+  )
+}
+
+export default MenuItemArticleCard

85d2005dfb13de9030c8652a822d3d580860fd72
Adam <jakubicka.adam@gmail.com>
2024-05-06 19:04:11 +0200

    Implement i18n translations for storybook

--- next/.storybook/preview.tsx
+++ next/.storybook/preview.tsx
@@ -1,7 +1,21 @@
 import type { Preview } from '@storybook/react'
 import '@/styles/globals.css'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n'
+import React from 'react'
+
+// addDecorator(storyFn => (
+//   <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>
+// ));
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <I18nextProvider i18n={i18n}>
+        <Story />
+      </I18nextProvider>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {

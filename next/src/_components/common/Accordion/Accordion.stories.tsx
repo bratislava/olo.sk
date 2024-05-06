502cdc089eb6b023a7930cd2ca8a697b87b2a840
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2024-05-06 15:11:39 +0200

    Accordion #54 (#98)
    * Add Accordion
    
    * Add AccordionGroup
    
    * Fix small things in Accordion
    
    * Add an alternative AccordionNew with tailwind-only animations
    
    * Revert "Add an alternative AccordionNew with tailwind-only animations"
    
    This reverts commit 19412694cb32bdf19e9760befdfc97f3eac96698.
    
    * Change padding of Accordion content

---
+++ next/src/_components/common/Accordion/Accordion.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Typography from '@/_components/common/Typography/Typography'
+
+import AccordionComponent from './Accordion'
+
+const meta: Meta<typeof AccordionComponent> = {
+  component: AccordionComponent,
+  title: 'Components/Accordion/Accordion',
+  tags: ['autodocs'],
+  args: {
+    title: 'AccordionComponent title',
+    children: (
+      <Typography variant="p-default">
+        Ut eget malesuada nisl. Donec gravida, risus in maximus tincidunt, augue elit maximus ante,
+        at posuere arcu est in metus. Sed maximus risus nulla, quis blandit nulla efficitur in.
+        Aenean porttitor lacus sed gravida volutpat. Nunc feugiat quam id mi pulvinar vestibulum.
+        Integer eget porttitor est. Duis at enim sit amet tellus elementum vehicula nec quis enim.
+        Curabitur ut molestie ex. Mauris aliquet vulputate lacus, sed mollis urna luctus in.
+        Phasellus magna neque, aliquet nec odio at, accumsan ultricies justo. Suspendisse sit amet
+        orci purus.
+      </Typography>
+    ),
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<typeof AccordionComponent>
+
+export const Accordion: Story = {
+  args: {},
+  render: (args) => (
+    <div className="w-full max-w-[500px]">
+      <AccordionComponent {...args} />
+    </div>
+  ),
+}

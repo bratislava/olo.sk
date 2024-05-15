597ba6fc31366efc6802bd55097ae706f84f0eac
Adam <jakubicka.adam@gmail.com>
2024-05-15 11:07:25 +0200

    Small fix in CardBase

--- next/src/_components/common/Card/CardBase.tsx
+++ next/src/_components/common/Card/CardBase.tsx
@@ -26,8 +26,8 @@ const CardBase = ({
         cx(
           'group/CardBase relative flex flex-col overflow-hidden',
           // When card is focused, we want to hide all rings of descendants and ring-0 still shows something
-          // This needs to be changed when we'll need more focusable elements inside a card
-          'focus-within:ring [&_*]:outline-none [&_*]:ring-transparent [&_*]:ring-offset-transparent',
+          // This needs to be revisited when we'll need more focusable elements inside a card
+          'ring-offset-2 focus-within:ring [&_*]:outline-none [&_*]:ring-transparent [&_a]:ring-offset-transparent',
           {
             'rounded-lg bg-background-primary': variant !== 'unstyled',
 

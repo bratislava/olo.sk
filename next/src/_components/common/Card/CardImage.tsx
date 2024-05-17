3bd88b402da754f9589a7c858a02ecc59f191294
Adam <jakubicka.adam@gmail.com>
2024-05-17 14:20:15 +0200

    Add index file for image assets to have more practical access to images in Storybook

--- next/src/_components/common/Card/CardImage.tsx
+++ next/src/_components/common/Card/CardImage.tsx
@@ -4,16 +4,16 @@ import { twMerge } from 'tailwind-merge'
 import ImagePlaceholder from '@/_components/common/ImagePlaceholder'
 import { generateImageSizes } from '@/_utils/generateImageSizes'
 
-type ArticleCardProps = {
+type CardImageProps = {
   imgSrc?: string
   className?: string // usually used to set aspect-ratio and corner radius
 }
 
 const imageSizes = generateImageSizes({ default: '100vw', md: '50vw', lg: '33vw' })
 
-const ArticleCard = ({ imgSrc, className }: ArticleCardProps) => {
+const CardImage = ({ imgSrc, className }: CardImageProps) => {
   return (
-    <div className={twMerge('relative shrink-0 overflow-hidden', className)}>
+    <div className={twMerge('relative size-full shrink-0 overflow-hidden', className)}>
       {imgSrc ? (
         <Image src={imgSrc} sizes={imageSizes} alt="" fill className="object-cover" />
       ) : (
@@ -23,4 +23,4 @@ const ArticleCard = ({ imgSrc, className }: ArticleCardProps) => {
   )
 }
 
-export default ArticleCard
+export default CardImage
---
+++ next/src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export { default as OloTruckImage } from './olo-truck.jpg'
+export { default as AbstractBackgroundImage } from './placeholder-abstract-background.png'
+export { default as ZevoIronHandImage } from './zevo-iron-hand.jpg'

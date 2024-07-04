af789e1750cd77f61df355ccc542fe0a5c50d0d5
Adam <jakubicka.adam@gmail.com>
2024-07-04 20:03:09 +0200

    Refactor BasicRowCard and add CareerRowCard

--- next/src/components/common/Card/BasicRowCard.stories.tsx
+++ next/src/components/common/Card/BasicRowCard.stories.tsx
@@ -1,39 +1,65 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import KalendarIconUrl from '@/src/assets/icons/kalendar.svg?url'
+import Icon, { iconNameMap } from '@/src/components/common/Icon/Icon'
 
-import BasicRowCardComponent from './BasicRowCard'
+import BasicRowCardComponent, { BasicRowCardProps } from './BasicRowCard'
 
-const meta: Meta<typeof BasicRowCardComponent> = {
+type Props = BasicRowCardProps & {
+  iconName: keyof typeof iconNameMap
+}
+
+const meta: Meta<Props> = {
   component: BasicRowCardComponent,
   title: 'Components/Cards/BasicRowCard',
   args: {
-    value: 'value',
-    variant: 'icon-label-value-vertical',
-    label: 'label',
-    iconSrc: KalendarIconUrl,
-    hasBottomBorder: true,
-    toolTipText: 'tooltip text',
+    label: 'Label',
+    value: 'Value',
+    iconName: 'hodiny',
   },
-  parameters: { controls: { exclude: ['className'] } },
-  argTypes: { variant: { control: { type: 'inline-radio' } } },
+  parameters: { controls: { exclude: ['className', 'variant', 'icon'] } },
+  argTypes: {
+    variant: { control: { type: 'inline-radio' } },
+    iconName: {
+      name: 'icon (name)',
+      options: Object.keys(iconNameMap),
+      control: { type: 'select' },
+    },
+  },
   tags: ['autodocs'],
 }
 
 export default meta
-type Story = StoryObj<typeof BasicRowCardComponent>
+type Story = StoryObj<Props>
 
 export const BasicRowCard: Story = {
-  render: (args) => <BasicRowCardComponent {...args} />,
-}
-
-export const BasicRowCardRows: Story = {
-  render: (args) => (
-    <div className="flex flex-col">
-      <BasicRowCardComponent {...args} />
-      <BasicRowCardComponent {...args} />
-      <BasicRowCardComponent {...args} />
-      <BasicRowCardComponent {...args} hasBottomBorder={false} />
-    </div>
-  ),
+  render: (args: any) => {
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
+          <p className="text-[0.6rem]">variant: icon-value</p>
+          <BasicRowCardComponent
+            variant="icon-value"
+            value={args.value}
+            icon={<Icon name={args.iconName} />}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="text-[0.6rem]">variant: label-value-vertical</p>
+          <BasicRowCardComponent
+            variant="label-value-vertical"
+            label={args.label ?? ''}
+            value={args.value}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="text-[0.6rem]">variant: label-value-horizontal</p>
+          <BasicRowCardComponent
+            variant="label-value-horizontal"
+            label={args.label ?? ''}
+            value={args.value}
+          />
+        </div>
+      </div>
+    )
+  },
 }
--- next/src/components/common/Card/BasicRowCard.tsx
+++ next/src/components/common/Card/BasicRowCard.tsx
@@ -1,32 +1,22 @@
-import Image from 'next/image'
+import { ReactNode } from 'react'
 
 import CardBase from '@/src/components/common/Card/CardBase'
-import Icon from '@/src/components/common/Icon/Icon'
 import Typography from '@/src/components/common/Typography/Typography'
 import cn from '@/src/utils/cn'
 
-type BasicRowCardProps = {
+export type BasicRowCardProps = {
   value: string
-  hasBottomBorder?: boolean
   className?: string
 } & (
   | {
-      variant: 'icon-label-value-vertical'
-      label: string
-      iconSrc: string
-      toolTipText?: string
-    }
-  | {
       variant: 'label-value-horizontal' | 'label-value-vertical'
       label: string
-      iconSrc?: never
-      toolTipText?: never
+      icon?: never
     }
   | {
       variant: 'icon-value'
       label?: never
-      iconSrc: string
-      toolTipText?: never
+      icon: ReactNode
     }
 )
 
@@ -34,65 +24,29 @@ type BasicRowCardProps = {
  * Figma: https://www.figma.com/design/2qF09hDT9QNcpdztVMNAY4/OLO-Web?node-id=1341-9981&m=dev
  */
 
-const BasicRowCard = ({
-  variant,
-  hasBottomBorder = false,
-  value,
-  label,
-  iconSrc,
-  toolTipText,
-  className,
-}: BasicRowCardProps) => {
+const BasicRowCard = ({ variant, value, label, icon, className }: BasicRowCardProps) => {
   return (
     <CardBase variant="unstyled" className={cn('bg-background-primary', className)}>
-      <div
-        className={cn('px-3 lg:px-5', {
-          // eslint-disable-next-line sonarjs/no-duplicate-string
-          'border-b border-border-default': hasBottomBorder && variant !== 'label-value-vertical',
-        })}
-      >
-        <div
-          className={cn('py-3 lg:py-4', {
-            'border-b border-border-default': hasBottomBorder && variant === 'label-value-vertical',
-          })}
-        >
-          {variant === 'icon-label-value-vertical' ? (
-            <div className="flex gap-4">
-              <div className="relative size-6 shrink-0">
-                <Image src={iconSrc} alt="" fill className="object-contain" />
-              </div>
-              <div className="flex grow flex-col gap-1 lg:gap-2">
-                <Typography variant="p-default-black">{label}</Typography>
-                <Typography variant="p-default">{value}</Typography>
-              </div>
-              {/* TODO Consider using Tooltip component */}
-              {toolTipText ? (
-                <div title={toolTipText}>
-                  <Icon name="pomoc" className="size-5 lg:size-6" />
-                </div>
-              ) : null}
-            </div>
-          ) : variant === 'icon-value' ? (
-            <div className="flex gap-3 lg:gap-4">
-              <div className="relative size-5 lg:size-6">
-                <Image src={iconSrc} alt="" fill className="object-contain" />
-              </div>
-              <Typography variant="p-default">{value}</Typography>
-            </div>
-          ) : variant === 'label-value-horizontal' ? (
-            <div className="flex gap-4 lg:gap-6 [&>*]:grow">
-              <Typography variant="p-default-black">{label}</Typography>
-              <Typography variant="p-default" className_onlyWhenNecessary="text-right">
-                {value}
-              </Typography>
-            </div>
-          ) : variant === 'label-value-vertical' ? (
-            <div className="flex flex-col gap-1 lg:gap-2">
-              <Typography variant="p-default-black">{label}</Typography>
-              <Typography variant="p-default">{value}</Typography>
-            </div>
-          ) : null}
-        </div>
+      <div className="py-3 lg:py-4">
+        {variant === 'icon-value' ? (
+          <div className="flex gap-3 lg:gap-4">
+            {/* Icon will be passed from parent, but we want to ensure the correct size */}
+            <div className="size-5 lg:size-6 [&>*]:w-full">{icon}</div>
+            <Typography variant="p-default">{value}</Typography>
+          </div>
+        ) : variant === 'label-value-horizontal' ? (
+          <div className="flex gap-4 lg:gap-6 [&>*]:grow">
+            <Typography variant="p-default-black">{label}</Typography>
+            <Typography variant="p-default" className_onlyWhenNecessary="text-right">
+              {value}
+            </Typography>
+          </div>
+        ) : variant === 'label-value-vertical' ? (
+          <div className="flex flex-col gap-1 lg:gap-2">
+            <Typography variant="p-default-black">{label}</Typography>
+            <Typography variant="p-default">{value}</Typography>
+          </div>
+        ) : null}
       </div>
     </CardBase>
   )

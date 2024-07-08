b245daddcf25f6e27863cd57a99ac517a7d69a32
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-08 14:29:16 +0200

    Implement ServiceCard component

---
+++ next/src/components/common/Card/ServiceCard.tsx
@@ -0,0 +1,77 @@
+import { useTranslation } from 'next-i18next'
+
+import Badge from '@/src/components/common/Badge/Badge'
+import Button from '@/src/components/common/Button/Button'
+import CardBase from '@/src/components/common/Card/CardBase'
+import Icon from '@/src/components/common/Icon/Icon'
+import Typography from '@/src/components/common/Typography/Typography'
+import cn from '@/src/utils/cn'
+
+/**
+ * pass `children` prop - BadgesGroup / Badges OR render in all badges here
+ */
+
+// 336
+
+type ServiceCardProps = {
+  title: string
+  linkHref: string
+  hasWhiteBackground: boolean
+  className?: string
+}
+
+/**
+ * Figma: https://www.figma.com/design/2qF09hDT9QNcpdztVMNAY4/OLO-Web?node-id=2094-19259&t=KpDlPbubYbVcw7ey-4&mode=dev
+ */
+
+const ServiceCard = ({ title, linkHref, hasWhiteBackground, className }: ServiceCardProps) => {
+  const { t } = useTranslation() // title props sa nepreklada
+
+  return (
+    <CardBase
+      variant="background-white"
+      hasWhiteSectionBackground={hasWhiteBackground}
+      className={cn(className)}
+    >
+      {/* Plus Button */}
+      <div className="flex w-64 flex-col gap-8 lg:w-[21rem] lg:gap-12">
+        {/* Content */}
+        <div className="flex flex-col items-start justify-start gap-3 lg:gap-4">
+          <Typography
+            variant="h6"
+            className_onlyWhenNecessary="line-clamp-3 group-hover/CardBase:underline"
+          >
+            {title}
+          </Typography>
+
+          {/* Badges: here, or extract later */}
+          <div className="flex h-12 flex-wrap items-start justify-start gap-2">
+            <Badge variant="public" />
+            <Badge variant="firms" />
+            <Badge variant="institutions" />
+          </div>
+        </div>
+
+        {/* Button */}
+        <div className="flex items-center gap-6">
+          <Button
+            variant="black-link"
+            asLink
+            href={linkHref}
+            stretched
+            hasLinkIcon={false}
+            className="font-bold no-underline"
+          >
+            <Typography variant="button-default">{t('common.findOutMore')}</Typography>
+          </Button>
+          <div className="ml-auto flex size-10 items-center justify-center rounded-lg bg-background-secondary">
+            <Icon name="sipka-doprava" className="size-6" />
+          </div>
+        </div>
+        {/* Button */}
+      </div>
+    </CardBase>
+  )
+}
+
+export default ServiceCard

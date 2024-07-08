fffd00c99249789f433f0745c72dc78361cd602b
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-08 22:45:55 +0200

    Implement suggested changes

--- next/src/components/common/Card/ServiceCard.tsx
+++ next/src/components/common/Card/ServiceCard.tsx
@@ -19,7 +19,7 @@ type ServiceCardProps = {
  */
 
 const ServiceCard = ({ title, linkHref, hasWhiteBackground, className }: ServiceCardProps) => {
-  const { t } = useTranslation() // title props sa nepreklada
+  const { t } = useTranslation()
 
   return (
     <CardBase
@@ -27,7 +27,7 @@ const ServiceCard = ({ title, linkHref, hasWhiteBackground, className }: Service
       hasWhiteSectionBackground={hasWhiteBackground}
       className={cn(className)}
     >
-      <div className="flex w-64 flex-col gap-8 lg:w-[21rem] lg:gap-12">
+      <div className="flex flex-col gap-8 lg:gap-12">
         <div className="flex flex-col items-start justify-start gap-3 lg:gap-4">
           <Typography
             variant="h6"
@@ -35,7 +35,7 @@ const ServiceCard = ({ title, linkHref, hasWhiteBackground, className }: Service
           >
             {title}
           </Typography>
-          <div className="flex h-12 flex-wrap items-start justify-start gap-2">
+          <div className="flex flex-wrap items-start justify-start gap-2">
             <Badge variant="public" />
             <Badge variant="firms" />
             <Badge variant="institutions" />
@@ -50,7 +50,7 @@ const ServiceCard = ({ title, linkHref, hasWhiteBackground, className }: Service
             hasLinkIcon={false}
             className="font-bold no-underline"
           >
-            <Typography variant="button-default">{t('common.findOutMore')}</Typography>
+            {t('common.findOutMore')}
           </Button>
           <div className="ml-auto flex size-10 items-center justify-center rounded-lg bg-background-secondary">
             <Icon name="sipka-doprava" className="size-6" />

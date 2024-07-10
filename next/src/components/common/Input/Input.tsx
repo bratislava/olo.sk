ef9f4b8896b7187bdb70c4d89b478de212f7fbd7
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-10 15:36:17 +0200

    Change Input component in order to style Pagination

--- next/src/components/common/Input/Input.tsx
+++ next/src/components/common/Input/Input.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
   Input as RACInput,
   Label as RACLabel,
@@ -15,13 +16,16 @@ export interface InputProps
 const Input = ({ labelText, className, ...restProps }: InputProps) => {
   return (
     <RACTextField className={cn('flex flex-col gap-2', className)}>
-      <RACLabel>
-        <Typography variant="h6">{labelText}</Typography>
-      </RACLabel>
+      {labelText ? (
+        <RACLabel>
+          <Typography variant="h6">{labelText}</Typography>
+        </RACLabel>
+      ) : null}
       <RACInput
         className={cn(
           'rounded-lg border border-border-default px-4 py-3 outline-none',
           'ring-offset-2 transition hover:border-border-hover focus:border-content-secondary focus-visible:ring',
+          className,
         )}
         {...restProps}
       />

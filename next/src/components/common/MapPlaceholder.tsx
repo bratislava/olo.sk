9406329bf475aa2f5f55ce20baa0710daeca0c67
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-12 12:05:17 +0200

    Create temporary placeholder in place of a Mapbox map

---
+++ next/src/components/common/MapPlaceholder.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+import mapImage from '@/src/assets/images/olo-placeholder-map.png'
+
+const MapPlaceholder = () => {
+  return (
+    <Image
+      src={mapImage}
+      alt="Mapa"
+      width={324}
+      height={220}
+      className="rounded-t-lg lg:rounded-l-lg lg:rounded-t-none"
+    />
+  )
+}
+
+export default MapPlaceholder

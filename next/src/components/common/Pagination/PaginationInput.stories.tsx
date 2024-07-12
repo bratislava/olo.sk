62590e3a40fc8e1fd20343768e2b7e288f5ed397
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-12 16:04:51 +0200

    Create a story

---
+++ next/src/components/common/Pagination/PaginationInput.stories.tsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useArgs } from '@storybook/preview-api'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import PaginationInputComponent from './PaginationInput'
+
+const meta: Meta<typeof PaginationInputComponent> = {
+  component: PaginationInputComponent,
+  title: 'Components/PaginationInput',
+  tags: ['autodocs'],
+  args: {
+    currentPage: 1,
+    totalCount: 20,
+  },
+  parameters: { controls: { exclude: ['onPageChange'] } },
+  decorators: [
+    (Story) => (
+      <div className="flex justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<typeof PaginationInputComponent>
+
+export const PaginationInput: Story = {
+  render: (props) => {
+    const [{ currentPage }, updateArgs] = useArgs()
+
+    return (
+      <PaginationInputComponent
+        {...props}
+        currentPage={currentPage}
+        onPageChange={(num) => updateArgs({ currentPage: num })}
+      />
+    )
+  },
+}

65929d319e3ab8dd2e9608589d3e8e1251a6e4d1
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-07-16 13:34:00 +0200

    #217 Change Router to Sitemap content type (#234)

--- next/src/pages/api/paths-list.ts
+++ next/src/pages/api/paths-list.ts
@@ -4,11 +4,11 @@ import { client } from '@/src/services/graphql'
 import { getFullPathFn } from '@/src/utils/useGetFullPath'
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const { router } = await client.General({ locale: 'sk' }) // TODO locale
+  const { sitemap } = await client.General({ locale: 'sk' }) // TODO locale
 
   const articlesPath = getFullPathFn(
-    router?.data?.attributes?.articlesParentPage?.data,
-    router?.data,
+    sitemap?.data?.attributes?.articlesParentPage?.data,
+    sitemap?.data,
   )
 
   return res.json({ articlesPath })

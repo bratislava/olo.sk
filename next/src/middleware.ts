574003707b7285173189de5786e47ebb6de4191c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-07-17 04:40:47 +0200

    Move logic to api handler

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -15,27 +15,29 @@ export const config = {
 }
 
 /**
- * This middleware rewrites path based on data from Strapi "Router" content type.
- * It requests data through api endpoint, to be able to use our graphql client
- * (it's not possible to use graphql client, because it depends on node and middleware uses edge runtime instead of node)
+ * This middleware rewrites path based on given path and data from Strapi "Sitemap" content type.
+ * The logic of rewrites lives in api handler, to be able to use our graphql client.
+ * (it's not possible to use graphql client in middleware, because it depends on node runtime and middleware uses edge runtime)
  *
  * @param request
  */
 // eslint-disable-next-line consistent-return
 export async function middleware(request: NextRequest) {
-  // eslint-disable-next-line no-console
-  console.log('request.nextUrl', request.nextUrl)
-  console.log('request.url', request.url)
-  const response = await fetch(`${request.nextUrl.origin}/api/paths-list`)
-  const { articlesPath } = await response.json()
+  // TODO remove console logs when we will be sure it works as intended
+  /**
+   * The request is handled on server, so we have to use http instead of https.
+   * `pathname` contains leading "/"
+   */
+  const fetchUrl = `http://${request.nextUrl.host}/api/sitemap${request.nextUrl.pathname}`
+  console.log('Middleware: api fetchUrl', fetchUrl)
 
-  // TODO add other content types
-  if (request.nextUrl.pathname.startsWith(`${articlesPath}/`)) {
-    const newUrl = new URL(
-      `/articles/${request.nextUrl.pathname.split('/').at(-1)}`,
-      request.nextUrl.origin,
-    )
-    console.log('new url', newUrl.toString())
+  const response = await fetch(fetchUrl)
+  const { destination } = await response.json()
+  console.log('Middleware: destination', destination)
+
+  if (typeof destination === 'string' && destination.length > 0) {
+    const newUrl = new URL(destination, request.nextUrl.origin)
+    console.log('Middleware: newUrl', newUrl.toString())
 
     return NextResponse.rewrite(newUrl)
   }

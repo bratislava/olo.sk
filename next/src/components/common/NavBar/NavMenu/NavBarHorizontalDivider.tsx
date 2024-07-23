9cc685e3352a0bbb5b35e7922e5c771f4923f3cf
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-23 14:03:00 +0200

    Add NavBarHorizontalDivider

---
+++ next/src/components/common/NavBar/NavMenu/NavBarHorizontalDivider.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const NavBarHorizontalDivider = () => {
+  return <div aria-hidden className="border-black border-l" />
+}
+
+export default NavBarHorizontalDivider

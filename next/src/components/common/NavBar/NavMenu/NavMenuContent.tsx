89fd47036028e1b8ed0272500537e5b1bf8a210e
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-29 23:43:09 +0200

    Replace NavBarPlaceholder with the most recent menu implementation

---
+++ next/src/components/common/NavBar/NavMenu/NavMenuContent.tsx
@@ -0,0 +1,33 @@
+import { NavigationMenuContent } from '@radix-ui/react-navigation-menu'
+
+import { MenuLinkFragment, MenuSectionFragment } from '@/src/services/graphql/api'
+import cn from '@/src/utils/cn'
+
+type NavMenuContentProps = {
+  sections: MenuSectionFragment[]
+  seeAllLink: MenuLinkFragment
+  className?: string
+}
+
+const NavMenuContent = ({ sections, seeAllLink, className }: NavMenuContentProps) => {
+  return (
+    <NavigationMenuContent
+      // To disable "onHover" behaviour, needs to be set also in NavMenuTrigger
+      // https://github.com/radix-ui/primitives/issues/1630#issuecomment-1237106380
+      onPointerMove={(event) => event.preventDefault()}
+      onPointerLeave={(event) => event.preventDefault()}
+      className="w-screen"
+    >
+      <div
+        className={cn(
+          'relative z-[29] flex flex-col items-start justify-start bg-background-primary px-28 shadow',
+          className,
+        )}
+      >
+        {/* TODO: Add sections */}
+      </div>
+    </NavigationMenuContent>
+  )
+}
+
+export default NavMenuContent
---
+++ next/src/components/common/NavBar/NavMenu/getParsedMenus.ts
@@ -0,0 +1,6 @@
+import { MenuEntityFragment } from '@/src/services/graphql/api'
+
+// TODO: Temporary solution
+export const getParsedMenus = (menu: MenuEntityFragment) => {
+  return null
+}

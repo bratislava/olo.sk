a86b5f8006c339226ad314c65e41df7ba8ca8f6c
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2024-07-29 16:06:15 +0200

    Add GeneralContextProvider to storybook (#259)

--- next/.storybook/preview.tsx
+++ next/.storybook/preview.tsx
@@ -3,17 +3,24 @@ import '@/src/styles/globals.css'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
 import React from 'react'
+import { GeneralContextProvider } from '../src/providers/GeneralContextProvider'
+import { client } from '../src/services/graphql'
+import BAQueryClientProvider from '../src/providers/BAQueryClientProvider'
 
-// addDecorator(storyFn => (
-//   <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>
-// ));
+export const loaders = [
+  async () => ({
+    general: await client.General({ locale: 'sk' }),
+  }),
+]
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <I18nextProvider i18n={i18n}>
-        <Story />
-      </I18nextProvider>
+    (Story, { loaded: general }) => (
+      <GeneralContextProvider general={general}>
+        <I18nextProvider i18n={i18n}>
+          <Story />
+        </I18nextProvider>
+      </GeneralContextProvider>
     ),
   ],
   parameters: {

41ae02118515631b59cf6d51a227de9f0b3a70c2
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2024-07-29 16:05:43 +0200

    #256 Fix CardBase Height and Focus ring (#263)
    Fix CardBase

--- next/src/components/common/Card/CardBase.tsx
+++ next/src/components/common/Card/CardBase.tsx
@@ -26,7 +26,7 @@ const CardBase = ({
         'group/CardBase relative flex flex-col overflow-hidden',
         // When card is focused, we want to hide all rings of descendants and ring-0 still shows something
         // This needs to be revisited when we'll need more focusable elements inside a card
-        'ring-offset-2 focus-within:ring [&_*]:outline-none [&_*]:ring-transparent [&_a]:ring-offset-transparent',
+        'ring-offset-2 focus-within:[&:has(:focus-visible)]:ring [&_*]:outline-none [&_*]:ring-transparent [&_a]:ring-offset-transparent',
         {
           'rounded-lg bg-background-primary': variant !== 'unstyled',
 

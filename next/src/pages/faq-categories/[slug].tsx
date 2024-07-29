9b6e46df2ee3263b48236baa032566d21ad4ce77
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-07-29 17:05:19 +0200

    Introduce detail pages for remaining content types

---
+++ next/src/pages/faq-categories/[slug].tsx
@@ -0,0 +1,88 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import * as React from 'react'
+
+import SectionContainer from '@/src/components/layout/Section/SectionContainer'
+import PageLayoutPlaceholder from '@/src/components/placeholder/PageLayoutPlaceholder'
+import HeaderTitleText from '@/src/components/sections/headers/HeaderTitleText'
+import { GeneralContextProvider } from '@/src/providers/GeneralContextProvider'
+import { client } from '@/src/services/graphql'
+import { FaqCategoryEntityFragment, GeneralQuery } from '@/src/services/graphql/api'
+
+type PageProps = {
+  general: GeneralQuery
+  entity: FaqCategoryEntityFragment
+}
+
+type StaticParams = {
+  slug: string
+}
+
+export const getStaticPaths: GetStaticPaths<StaticParams> = async () => {
+  // TODO return all paths
+
+  return { paths: [], fallback: 'blocking' }
+}
+
+export const getStaticProps: GetStaticProps<PageProps, StaticParams> = async ({
+  locale,
+  params,
+}) => {
+  const slug = params?.slug
+
+  // TODO update console log so it displays correct path
+  // eslint-disable-next-line no-console
+  console.log(`Revalidating Workshop ${locale === 'en' ? '/en' : ''}/documents/${slug}`)
+
+  // TODO || !locale
+  if (!slug || !locale) {
+    return { notFound: true }
+  }
+
+  const [{ faqCategories: entities }, general, translations] = await Promise.all([
+    client.FaqCategoryBySlug({ slug, locale }),
+    client.General({ locale }),
+    serverSideTranslations(locale),
+  ])
+
+  const entity = entities?.data[0]
+  if (!entity) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      entity,
+      general,
+      ...translations,
+    },
+    revalidate: 10,
+  }
+}
+
+const Page = ({ entity, general }: PageProps) => {
+  if (!entity.attributes) {
+    return null
+  }
+
+  const { title } = entity.attributes
+
+  return (
+    <GeneralContextProvider general={general}>
+      {/* TODO common Head/Seo component */}
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <PageLayoutPlaceholder>
+        {/* TODO Header and Content */}
+        <SectionContainer background="secondary">
+          <HeaderTitleText title={title} />
+        </SectionContainer>
+      </PageLayoutPlaceholder>
+    </GeneralContextProvider>
+  )
+}
+
+export default Page

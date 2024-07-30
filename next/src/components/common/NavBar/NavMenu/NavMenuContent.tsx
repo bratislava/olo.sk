6d852ca598aa284b7463d84ec298c77871766c0c
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-30 13:38:44 +0200

    Create styles for NavMenuContent

--- next/src/components/common/NavBar/NavMenu/NavMenuContent.tsx
+++ next/src/components/common/NavBar/NavMenu/NavMenuContent.tsx
@@ -1,5 +1,8 @@
 import { NavigationMenuContent } from '@radix-ui/react-navigation-menu'
 
+import NavMenuContentDivider from '@/src/components/common/NavBar/NavMenu/NavMenuContentDivider'
+import NavMenuLink from '@/src/components/common/NavBar/NavMenu/NavMenuLink'
+import NavMenuSection from '@/src/components/common/NavBar/NavMenu/NavMenuSection'
 import { MenuLinkFragment, MenuSectionFragment } from '@/src/services/graphql/api'
 import cn from '@/src/utils/cn'
 
@@ -10,23 +13,43 @@ type NavMenuContentProps = {
 }
 
 const NavMenuContent = ({ sections, seeAllLink, className }: NavMenuContentProps) => {
+  // TODO: Parse/group menuCells into groupedSections
+
   return (
     <NavigationMenuContent
       // To disable "onHover" behaviour, needs to be set also in NavMenuTrigger
       // https://github.com/radix-ui/primitives/issues/1630#issuecomment-1237106380
       onPointerMove={(event) => event.preventDefault()}
       onPointerLeave={(event) => event.preventDefault()}
-      className="w-screen"
+      className="w-screen bg-background-primary"
     >
       <div
         className={cn(
-          'relative z-[29] flex flex-col items-start justify-start bg-background-primary px-28 shadow',
+          'relative z-[29] flex flex-col items-start justify-start px-28 shadow',
           className,
         )}
       >
-        {/* TODO: Temporary solution - add proper sections */}
-        {sections?.map((section: any) => section && <p>{section?.label}</p>)}
-        {seeAllLink?.label}
+        {/* Menu sections - TODO: Do not hard-code `grid-cols-3` */}
+        <ul
+          className="grid w-full grid-cols-3 gap-8 py-8"
+          // Together with onCLick in Viewport, it closes the menu on click outside of container area
+          onClick={(event) => event.stopPropagation()}
+        >
+          {sections?.map((section: any, index: number) => {
+            return (
+              <div className="flex gap-8">
+                {/* eslint-disable-next-line react/no-array-index-key */}
+                {index > 0 && <NavMenuContentDivider variant="horizontal" />}
+                {/* eslint-disable-next-line react/no-array-index-key */}
+                <NavMenuSection key={index} section={section} />
+              </div>
+            )
+          })}
+        </ul>
+        {/* seeAllLink */}
+        <div className="flex w-full items-start justify-start py-6">
+          <NavMenuLink id={seeAllLink?.id} label={seeAllLink?.label} url={seeAllLink?.url} />
+        </div>
       </div>
     </NavigationMenuContent>
   )

13cfe54ec81b4f5bb035855ebaaf9be48e02103c
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-31 15:26:41 +0200

    Update getParsedMenus function

--- next/src/components/common/NavBar/NavMenu/getParsedMenus.ts
+++ next/src/components/common/NavBar/NavMenu/getParsedMenus.ts
@@ -5,28 +5,24 @@ export const getParsedMenus = (menu: MenuEntityResponse) => {
   return menu?.data?.attributes?.menuItems?.map((menuItem) => {
     if (!menuItem?.sections) return null
 
-    const menuItemId = menuItem.id
-    const menuItemLabel = menuItem.label
-    const menuSeeAllLink = menuItem.seeAllLink
+    const { id: menuItemId, label: menuItemLabel, seeAllLink: menuSeeAllLink, sections } = menuItem
 
     const menuSections =
-      menuItem.sections
+      sections
         .map((section) => {
           if (!section) return null
 
-          const sectionId = section.id
-          const sectionLabel = section.label
-          const sectionColSpan = section.colSpan
-          const sectionMulticolumnBehaviour = section.multicolumnBehaviour
-          const sectionHasDivider = section.hasDivider
-          const sectionSpecialSectionType = section.specialSectionType
+          const {
+            id: sectionId,
+            label: sectionLabel,
+            colSpan: sectionColSpan,
+            multicolumnBehaviour: sectionMulticolumnBehaviour,
+            hasDivider: sectionHasDivider,
+            specialSectionType: sectionSpecialSectionType,
+            links,
+          } = section
 
-          const sectionLinks =
-            section.links
-              ?.map((menuLink) => {
-                return menuLink
-              })
-              .filter((element) => isDefined(element)) ?? []
+          const sectionLinks = links?.filter(isDefined) ?? []
 
           return {
             id: sectionId,

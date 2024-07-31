42fb9f4c6355c9ceb7c6c854195f9caa5db0f14d
katarinazaprazna <katarina.zaprazna@bratislava.sk>
2024-07-31 15:10:50 +0200

    Update getParsedMenus function

--- next/src/components/common/NavBar/NavMenu/getParsedMenus.ts
+++ next/src/components/common/NavBar/NavMenu/getParsedMenus.ts
@@ -1,4 +1,5 @@
 import { MenuEntityResponse } from '@/src/services/graphql/api'
+import { isDefined } from '@/src/utils/isDefined'
 
 export const getParsedMenus = (menu: MenuEntityResponse) => {
   return menu?.data?.attributes?.menuItems?.map((menuItem) => {
@@ -8,30 +9,36 @@ export const getParsedMenus = (menu: MenuEntityResponse) => {
     const menuItemLabel = menuItem.label
     const menuSeeAllLink = menuItem.seeAllLink
 
-    const menuSections = menuItem.sections.map((section) => {
-      if (!section) return null
+    const menuSections =
+      menuItem.sections
+        .map((section) => {
+          if (!section) return null
 
-      const sectionId = section.id
-      const sectionLabel = section.label
-      const sectionColSpan = section.colSpan
-      const sectionMulticolumnBehaviour = section.multicolumnBehaviour
-      const sectionHasDivider = section.hasDivider
-      const sectionSpecialSectionType = section.specialSectionType
+          const sectionId = section.id
+          const sectionLabel = section.label
+          const sectionColSpan = section.colSpan
+          const sectionMulticolumnBehaviour = section.multicolumnBehaviour
+          const sectionHasDivider = section.hasDivider
+          const sectionSpecialSectionType = section.specialSectionType
 
-      const sectionLinks = section.links?.map((menuLink) => {
-        return menuLink
-      })
+          const sectionLinks =
+            section.links
+              ?.map((menuLink) => {
+                return menuLink
+              })
+              .filter((element) => isDefined(element)) ?? []
 
-      return {
-        id: sectionId,
-        label: sectionLabel,
-        colSpan: sectionColSpan,
-        multicolumnBehaviour: sectionMulticolumnBehaviour,
-        hasDivider: sectionHasDivider,
-        specialSectionType: sectionSpecialSectionType,
-        links: sectionLinks,
-      }
-    })
+          return {
+            id: sectionId,
+            label: sectionLabel,
+            colSpan: sectionColSpan,
+            multicolumnBehaviour: sectionMulticolumnBehaviour,
+            hasDivider: sectionHasDivider,
+            specialSectionType: sectionSpecialSectionType,
+            links: sectionLinks,
+          }
+        })
+        .filter((element) => isDefined(element)) ?? []
 
     return {
       id: menuItemId,

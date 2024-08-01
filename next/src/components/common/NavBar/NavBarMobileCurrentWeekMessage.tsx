7e9f25a0fd09c40fa5526341b1d5d4922bca398f
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-08-01 16:00:55 +0200

    Fix vertical spacing on mobile

--- next/src/components/common/NavBar/NavBarMobileCurrentWeekMessage.tsx
+++ next/src/components/common/NavBar/NavBarMobileCurrentWeekMessage.tsx
@@ -15,7 +15,7 @@ const MobileNavBarCurrentWeekMessage = ({
 }: MobileNavBarCurrentWeekMessageProps) => {
   return (
     <div className={cn(className)}>
-      <div aria-hidden className="h-[3.813rem]" />
+      <div aria-hidden className="h-[3.7rem]" />
       <SectionContainer className="w-full bg-action-background-default py-2 text-center">
         <Typography variant="p-small">{currentWeekMessage}</Typography>
       </SectionContainer>

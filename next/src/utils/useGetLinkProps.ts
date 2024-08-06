2ce5abece037371e1ed70d0015676a47a89e9993
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-08-06 20:32:52 +0200

    Extended getLinkProps

--- next/src/utils/useGetLinkProps.ts
+++ next/src/utils/useGetLinkProps.ts
@@ -22,6 +22,7 @@ export const useGetLinkProps = () => {
   const getLinkProps = (link: LinkFragment | MenuLinkFragment | null | undefined) => {
     let href = '#'
     let label = link?.label ?? ''
+    let subText = ''
     let target: '_blank' | undefined
 
     if (!link) {
@@ -37,9 +38,12 @@ export const useGetLinkProps = () => {
       href = getFullPath(link.article.data) ?? '#'
     } else if (link.branch?.data?.attributes) {
       label = link.label ?? link.branch.data.attributes.title
+      subText =
+        'address' in link.branch.data.attributes ? link.branch.data.attributes.address ?? '' : ''
       href = getFullPath(link.branch.data) ?? '#'
     } else if (link.workshop?.data?.attributes) {
       label = link.label ?? link.workshop.data.attributes.title
+      // TODO
       href = getFullPath(link.workshop.data) ?? '#'
     } else if ('document' in link && link.document?.data?.attributes) {
       label = link.label ?? link.document.data.attributes.title
@@ -54,7 +58,7 @@ export const useGetLinkProps = () => {
     //   ? { id: link.plausibleId }
     //   : undefined
 
-    return { children: label, href, target }
+    return { children: label, subText, href, target }
   }
 
   return { getLinkProps }

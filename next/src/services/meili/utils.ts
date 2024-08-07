054afa6c46bc9ea22982f930d45b8eee509529f8
Adam <jakubicka.adam@gmail.com>
2024-08-07 12:13:28 +0200

    Add support for meili entities in useGetFullPath and related files

--- next/src/services/meili/utils.ts
+++ next/src/services/meili/utils.ts
@@ -8,7 +8,8 @@ import { SearchIndexWrapped } from './types'
  */
 export const unwrapFromSearchIndex = <T extends string, K extends object>(type: T) => {
   return (response: SearchResponse<SearchIndexWrapped<T, K>>) => {
-    return { ...response, hits: response.hits.map((hit) => hit[type] as K) }
+    // we add type back to the response to distinguish between different types, for example when getting the path to the entity
+    return { ...response, type, hits: response.hits.map((hit) => hit[type] as K) }
   }
 }
 
--- next/src/utils/getPagePath.ts
+++ next/src/utils/getPagePath.ts
@@ -1,4 +1,5 @@
 import { PageEntityFragment } from '@/src/services/graphql/api'
+import { PageMeili } from '@/src/services/meili/types'
 
 export const getPagePath = (page: PageEntityFragment) => {
   const current = page
@@ -12,6 +13,19 @@ export const getPagePath = (page: PageEntityFragment) => {
   while (parentPage?.data?.attributes) {
     pathArray.push(parentPage.data.attributes.slug)
     parentPage = parentPage?.data?.attributes?.parentPage
+  }
+
+  return `/${pathArray.reverse().join('/')}`
+}
+
+export const getMeiliPagePath = (page: PageMeili) => {
+  const current = page
+  const pathArray = [current.slug]
+
+  let { parentPage } = page
+  while (parentPage) {
+    pathArray.push(parentPage.slug)
+    parentPage = parentPage.parentPage
   }
 
   return `/${pathArray.reverse().join('/')}`

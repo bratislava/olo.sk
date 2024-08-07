7405e9a728a973f4ae4bbc40a7a11b1672946cea
Adam <jakubicka.adam@gmail.com>
2024-08-07 19:33:41 +0200

    Add FaqCategoriesSection

--- next/src/components/layout/Sections.tsx
+++ next/src/components/layout/Sections.tsx
@@ -6,6 +6,7 @@ import CardsListSection from '@/src/components/sections/CardsListSection'
 import ColumnsListSection from '@/src/components/sections/ColumnsListSection'
 import ColumnsSection from '@/src/components/sections/ColumnsSection'
 import DividerSection from '@/src/components/sections/DividerSection'
+import FaqCategoriesSection from '@/src/components/sections/FaqCategoriesSection'
 import FaqSection from '@/src/components/sections/FaqSection'
 import FilesSection from '@/src/components/sections/FilesSection'
 import ImageAndTextOverlappedSection from '@/src/components/sections/ImageAndTextOverlappedSection'
@@ -64,6 +65,9 @@ const SectionContent = ({ section }: { section: PageSectionsFragment }) => {
 
     case 'ComponentSectionsCardsList':
       return <CardsListSection section={section} />
+
+    case 'ComponentSectionsFaqCategories':
+      return <FaqCategoriesSection section={section} />
 
     default:
       return null

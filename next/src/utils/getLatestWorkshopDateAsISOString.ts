f6d1d1c85bc2c12c245cbbbd21ddf5c995a181de
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-08-08 00:29:01 +0200

    Resolve schema issues

--- next/src/utils/getLatestWorkshopDateAsISOString.ts
+++ next/src/utils/getLatestWorkshopDateAsISOString.ts
@@ -1,6 +1,6 @@
-import { WorkshopSlugEntityFragment } from '@/src/services/graphql/api'
+import { WorkshopEntityFragment } from '@/src/services/graphql/api'
 
-export const getLatestWorkshopDateAsISOString = (workshop: WorkshopSlugEntityFragment) => {
+export const getLatestWorkshopDateAsISOString = (workshop: WorkshopEntityFragment) => {
   if (!workshop.attributes?.dates) return null
   const unixTimestamp = Math.min(
     ...workshop.attributes.dates.map((date) => new Date(date?.value).getTime()),
--- next/src/utils/useMostRecentWorkshopDate.ts
+++ next/src/utils/useMostRecentWorkshopDate.ts
@@ -1,12 +1,12 @@
 import { useTranslation } from 'next-i18next'
 
-import { WorkshopSlugEntityFragment } from '@/src/services/graphql/api'
+import { WorkshopEntityFragment } from '@/src/services/graphql/api'
 import { getLatestWorkshopDateAsISOString } from '@/src/utils/getLatestWorkshopDateAsISOString'
 
 export const useMostRecentWorkshopDate = () => {
   const { t } = useTranslation()
 
-  const getMostRecentWorkshopDateMessage = (workshop?: WorkshopSlugEntityFragment) => {
+  const getMostRecentWorkshopDateMessage = (workshop?: WorkshopEntityFragment) => {
     const dates = workshop?.attributes?.dates
     if (!dates) return { mostRecentDateMessage: null }
 

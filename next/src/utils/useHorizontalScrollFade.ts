a50114ddde6274c242c8ccb7ca0637578ac33884
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2024-08-08 14:10:01 +0200

    #324 Correct Markdown renderer styling, especially tables (#324)
    * Correct Markdown renderer styling, especially tables
    
    * Add file source attribution (Marianum) to useHorizontalScrollFade.ts

---
+++ next/src/utils/useHorizontalScrollFade.ts
@@ -0,0 +1,46 @@
+import cx from 'classnames'
+import { RefObject, useState } from 'react'
+import { useResizeDetector } from 'react-resize-detector'
+import { useEventListener } from 'usehooks-ts'
+
+// copied from Marianum: https://github.com/bratislava/marianum.sk/blob/master/next/utils/useHorizontalScrollFade.ts
+export const useHorizontalScrollFade = ({
+  ref,
+  classNameLeft = 'scroll-fade-left',
+  classNameLeftOpaque = 'scroll-fade-left-opaque',
+  classNameRight = 'scroll-fade-right',
+  classNameRightOpaque = 'scroll-fade-right-opaque',
+}: {
+  ref: RefObject<HTMLElement>
+  classNameLeft?: string
+  classNameLeftOpaque?: string
+  classNameRight?: string
+  classNameRightOpaque?: string
+}) => {
+  const [isScrolledLeft, setIsScrolledLeft] = useState(true)
+  const [isScrolledRight, setIsScrolledRight] = useState(true)
+
+  const handleScrollOrResize = () => {
+    if (!ref.current) {
+      return
+    }
+    const scrollableWidth = ref.current.scrollWidth
+    const viewportWidth = ref.current.clientWidth
+
+    setIsScrolledLeft(ref.current.scrollLeft === 0)
+    setIsScrolledRight(
+      ref.current.scrollLeft >= scrollableWidth - viewportWidth - 2 /* Can be off few pixels */,
+    )
+  }
+
+  // Also triggers the function on the mount, so no need for useEffect.
+  useResizeDetector({ targetRef: ref, onResize: () => handleScrollOrResize() })
+  useEventListener('scroll', handleScrollOrResize, ref)
+
+  return {
+    scrollFadeClassNames: cx(classNameLeft, classNameRight, {
+      [classNameRightOpaque]: !isScrolledRight,
+      [classNameLeftOpaque]: !isScrolledLeft,
+    }),
+  }
+}

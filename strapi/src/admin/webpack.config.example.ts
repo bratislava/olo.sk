26755285bf180ae009d4a70542428ccf7610bc02
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-08-13 13:54:36 +0200

    Fix typo

--- strapi/src/admin/webpack.config.example.ts
+++ strapi/src/admin/webpack.config.example.ts
@@ -1,4 +1,4 @@
-grexport default (config: any, webpack: any) => {
+export default (config: any, webpack: any) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config

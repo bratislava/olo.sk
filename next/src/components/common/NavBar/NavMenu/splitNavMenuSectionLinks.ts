9cee03c90fd527841aa9480b0c19c105c0909d11
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-08-20 18:45:51 +0200

    #354 Consolidate unordered list in Menu into a single element (#376)
    Consolidate unordered list into a single element

--- next/src/components/common/NavBar/NavMenu/splitNavMenuSectionLinks.ts
+++
@@ -1,8 +0,0 @@
-import { MenuLinkFragment } from '@/src/services/graphql/api'
-
-export const splitNavMenuSectionLinks = (links: MenuLinkFragment[]) => {
-  const firstSix = links.slice(0, 6)
-  const rest = links.slice(6)
-
-  return [firstSix, rest]
-}

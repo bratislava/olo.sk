2b6dd6dd07f98671ed65ec9f3d4161c1cd977695
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-08-22 15:34:36 +0200

    #403 Implement Contacts section FE

--- next/src/components/common/ContactBox.tsx
+++ next/src/components/common/ContactBox.tsx
@@ -5,24 +5,20 @@ import Typography from '@/src/components/common/Typography/Typography'
 
 type Props = {
   title: string
-  text: string
+  text?: string | null | undefined
   contacts: Pick<ContactRowCardProps, 'contact' | 'variant'>[]
 }
 
 const ContactBox = ({ title, text, contacts }: Props) => (
-  <div className="flex flex-col items-start rounded-lg border border-border-default">
+  <div className="flex flex-col rounded-lg border border-border-default">
     <div className="flex flex-col items-start gap-2 self-stretch border-b border-b-[#d5d4d4] px-6 pb-[1.375rem] pt-[1.375rem]">
       <Typography variant="h5" as="h3">
         {title}
       </Typography>
-      {text ? (
-        <Typography variant="h5" as="h3">
-          {text}
-        </Typography>
-      ) : null}
+      {text ? <Typography variant="p-default">{text}</Typography> : null}
     </div>
 
-    <div className="flex flex-col items-start px-6 py-2">
+    <div className="flex flex-col divide-y divide-border-default px-6 py-2">
       {contacts.map((contact, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <ContactRowCard key={index} {...contact} />

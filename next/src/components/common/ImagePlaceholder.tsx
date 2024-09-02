b1b27fa1eee41e78e09a07a2dd547e329b6603f8
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-09-02 09:36:53 +0200

    Add className to ImagePlaceholder.tsx

--- next/src/components/common/ImagePlaceholder.tsx
+++ next/src/components/common/ImagePlaceholder.tsx
@@ -1,12 +1,25 @@
 import Image from 'next/image'
 
 import oloTruckImage from '@/src/assets/images/olo-truck.jpg'
+import cn from '@/src/utils/cn'
 import { generateImageSizes } from '@/src/utils/generateImageSizes'
 
 const imageSizes = generateImageSizes({ default: '100vw', md: '50vw', lg: '33vw' })
 
-const ImagePlaceholder = () => {
-  return <Image src={oloTruckImage} sizes={imageSizes} alt="" fill className="object-cover" />
+type ImagePlaceholderProps = {
+  className?: string
+}
+
+const ImagePlaceholder = ({ className }: ImagePlaceholderProps) => {
+  return (
+    <Image
+      src={oloTruckImage}
+      sizes={imageSizes}
+      alt=""
+      fill
+      className={cn('object-cover', className)}
+    />
+  )
 }
 
 export default ImagePlaceholder

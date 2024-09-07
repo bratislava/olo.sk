021612183df7d9d229465b98a7a0988424d0d7b6
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-09-07 11:05:01 +0200

    #491 Cleanup fetch functions

--- next/src/services/navigation/fetchNavigation.ts
+++ next/src/services/navigation/fetchNavigation.ts
@@ -1,3 +1,5 @@
+import { fetchNonCachedNavigation } from '@/src/services/navigation/fetchNonCachedNavigation'
+import { fetchNonCachedNavigationFromApi } from '@/src/services/navigation/fetchNonCachedNavigationFromApi'
 import { ContentTypePathPrefixesMap } from '@/src/services/navigation/parseContentTypePathPrefixes'
 import { PagePathsMap } from '@/src/services/navigation/parseTopLevelPages'
 import { NavigationConfig, NavigationObject } from '@/src/services/navigation/typesNavigation'
@@ -13,29 +15,43 @@ let cache: {
   }
 } | null = null
 
-const fetchNonCached = async <Config extends NavigationConfig>(config: Config) => {
-  /**
-   * The request is handled on server, so we have to use http instead of https.
-   * `pathname` contains leading "/"
-   */
-  const fetchUrl = `http://${config.host}/api/navigation`
-
-  const response = await fetch(fetchUrl)
-
-  if (!response.ok) {
-    throw new Error(await response.text())
-  }
-
-  return response.json()
-}
-
+/**
+ * Fetches and cache navigation data directly from the Strapi client.
+ * Use this function in getStaticProps or getServerSideProps.
+ *
+ * @param config
+ */
 export const fetchNavigation = async <Config extends NavigationConfig>(config: Config) => {
   if (cache?.timestamp && cache.timestamp + config.cacheTtl > Date.now()) {
     return cache.value
   }
 
   // TODO types - make it type safe, now it's any
-  const navigation = (await fetchNonCached(config)) as NavigationObject
+  const navigation = (await fetchNonCachedNavigation()) as NavigationObject
+
+  const { contentTypePathPrefixesMap, pagePathsMap } = navigation
+
+  const value = { contentTypePathPrefixesMap, pagePathsMap }
+  cache = { timestamp: Date.now(), value }
+
+  return value
+}
+
+/**
+ * Fetches and cache navigation data from the Strapi client via API endpoint.
+ * Use this function in middleware.
+ * It's not possible to use graphql client in middleware, because it depends on node runtime and middleware uses edge runtime,
+ * so we fetch data through API endpoint.
+ *
+ * @param config
+ */
+export const fetchNavigationFromApi = async <Config extends NavigationConfig>(config: Config) => {
+  if (cache?.timestamp && cache.timestamp + config.cacheTtl > Date.now()) {
+    return cache.value
+  }
+
+  // TODO types - make it type safe, now it's any
+  const navigation = (await fetchNonCachedNavigationFromApi(config)) as NavigationObject
 
   const { contentTypePathPrefixesMap, pagePathsMap } = navigation
 

47bb154875cae8df9e84d08666555c62fa399fc1
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-09-23 13:04:46 +0200

    Set height for the video

--- next/src/components/common/Video/Video.tsx
+++ next/src/components/common/Video/Video.tsx
@@ -7,9 +7,11 @@ export type VideoProps = {
  * Figma: https://www.figma.com/file/2qF09hDT9QNcpdztVMNAY4/OLO-Web?node-id=1199%3A13348
  */
 
+// TODO: The video should scale responsively
+
 const Video = ({ url, ariaLabel }: VideoProps) => {
   return (
-    <div className="size-full overflow-hidden rounded-3xl">
+    <div className="h-[10.125rem] overflow-hidden rounded-3xl lg:h-[42.75rem]">
       <iframe
         width="100%"
         height="100%"

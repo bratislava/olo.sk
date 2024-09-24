a5cc09a9dc38402a7e55946c1c75ec5e9052fbda
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-09-24 11:16:34 +0200

    Make the video responsive

--- next/src/components/common/Video/Video.tsx
+++ next/src/components/common/Video/Video.tsx
@@ -7,17 +7,17 @@ export type VideoProps = {
  * Figma: https://www.figma.com/file/2qF09hDT9QNcpdztVMNAY4/OLO-Web?node-id=1199%3A13348
  */
 
-// TODO: The video should scale responsively
-
 const Video = ({ url, ariaLabel }: VideoProps) => {
   return (
-    <div className="h-[10.125rem] overflow-hidden rounded-3xl lg:h-[42.75rem]">
+    <div className="relative w-full overflow-hidden rounded-3xl pb-[56.25%]">
+      {/* pb-[56.25%] creates a 16:9 aspect ratio */}
       <iframe
         width="100%"
         height="100%"
         title={ariaLabel}
         src={url}
         allow="accelerometer; encrypted-media; gyroscope; web-share"
+        className="absolute left-0 top-0 size-full border-0"
       />
     </div>
   )

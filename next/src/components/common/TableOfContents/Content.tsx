c29144dad2d485a7d03c3bbf259b1df1ca27fec9
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-09-24 11:19:41 +0200

    Add highlight to selected item from list of content

--- next/src/components/common/TableOfContents/Content.tsx
+++ next/src/components/common/TableOfContents/Content.tsx
@@ -1,4 +1,5 @@
-import { handleOnClick } from '@/src/components/common/TableOfContents/utils'
+import { useState } from 'react'
+
 import Typography from '@/src/components/common/Typography/Typography'
 
 type HeadingProps = {
@@ -18,15 +19,30 @@ type Props = {
  */
 
 const Content = ({ headings, headerOffset }: Props) => {
+  const [activeId, setActiveId] = useState<string>()
+
+  const handleOnClick = (id: string) => {
+    const href = `#${id}`
+    const element = document.querySelector(href)
+    if (!element) return
+
+    const elementPosition = element?.getBoundingClientRect().top || 0 // current offset regarding to the current window scroll
+    const windowOffset = window.scrollY
+    const offsetPosition = elementPosition + windowOffset - headerOffset
+
+    setActiveId(id)
+    window.scrollTo({ top: offsetPosition, behavior: 'smooth' })
+  }
+
   return (
     <div key="table-of-contents" className="flex flex-col lg:px-6 lg:py-2">
       {headings &&
         headings.map((heading) => {
           return heading.level === 2 ? (
             <div
-              className="cursor-pointer py-3 hover:font-black lg:py-4"
+              className={`cursor-pointer py-3 hover:font-black ${heading.id === activeId ? 'font-black' : ''} lg:py-4`}
               key={heading.id}
-              onClick={() => handleOnClick({ id: `#${heading.id}`, headerOffset })}
+              onClick={() => handleOnClick(heading.id)}
               aria-hidden="true"
             >
               <Typography variant="p-default">{heading.text}</Typography>
@@ -34,8 +50,8 @@ const Content = ({ headings, headerOffset }: Props) => {
           ) : (
             <div key={heading.id} className="flex flex-col px-4 first:pt-4 last:pb-4">
               <div
-                className={`${heading.level === 3 ? 'pl-4' : 'pl-8'} flex cursor-pointer flex-col border-l border-border-default pb-2 hover:font-black lg:pb-3`}
-                onClick={() => handleOnClick({ id: `#${heading.id}`, headerOffset })}
+                className={`${heading.level === 3 ? 'pl-4' : 'pl-8'} flex cursor-pointer ${heading.id === activeId ? 'font-black' : ''} flex-col border-l border-border-default pb-2 hover:font-black lg:pb-3`}
+                onClick={() => handleOnClick(heading.id)}
                 aria-hidden="true"
                 key={heading.id}
               >

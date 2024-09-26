f666ee7c028cec81c41bd7545653dfa3c3b126b4
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-09-26 09:54:32 +0200

    Removing useEffect

--- next/src/components/common/TableOfContents/useHeadings.ts
+++ next/src/components/common/TableOfContents/useHeadings.ts
@@ -1,30 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useIsClient } from 'usehooks-ts'
 
 type HeadingLevels = 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
-type HeadingProps = {
-  id: string
-  text: string
-  level: number
-}
-
 const useHeadings = (rootId: string) => {
-  const [headings, setHeadings] = useState<HeadingProps[]>([])
+  const queryList = `#${rootId} h2, #${rootId} h3, #${rootId} h4, #${rootId} h5, #${rootId} h6`
+  const isClient = useIsClient()
 
-  useEffect(() => {
-    const queryList = `#${rootId} h2, #${rootId} h3, #${rootId} h4, #${rootId} h5, #${rootId} h6`
+  const getHeadings = () => {
     const headingsNodeList = document.querySelectorAll(queryList)
+
     // eslint-disable-next-line unicorn/prefer-spread
-    const headingsElements = Array.from(headingsNodeList).map((element) => ({
+    return Array.from(headingsNodeList).map((element) => ({
       id: element.id,
       text: element.textContent ?? '""',
       level: Number(String(element.tagName).slice(1).toLowerCase() as HeadingLevels),
     })) // mapping from a list of nodes to array
+  }
 
-    setHeadings(headingsElements)
-  }, [rootId])
-
-  return headings
+  return isClient ? getHeadings() : null
 }
 
 export default useHeadings

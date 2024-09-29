4ed8f764a7eeba8a2a21371f6dd8343ac2c564eb
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-09-29 21:17:32 +0200

    Create a new variant of basic row card to implement mobile menu trigger

--- next/src/components/common/Card/BasicRowCard.tsx
+++ next/src/components/common/Card/BasicRowCard.tsx
@@ -1,21 +1,33 @@
+import { ReactNode } from 'react'
+
 import CardBase from '@/src/components/common/Card/CardBase'
-import Icon, { IconName, isBaIcon } from '@/src/components/common/Icon/Icon'
-import OloIcon, { isOloIcon, OloIconName } from '@/src/components/common/Icon/OloIcon'
+import { IconName } from '@/src/components/common/Icon/Icon'
+import IconWrapper from '@/src/components/common/Icon/IconWrapper'
+import { OloIconName } from '@/src/components/common/Icon/OloIcon'
 import Typography from '@/src/components/common/Typography/Typography'
 import cn from '@/src/utils/cn'
 
 export type BasicRowCardProps = {
-  value: string
+  value?: string
+  label?: ReactNode
   className?: string
 } & (
   | {
+      value: string
       variant: 'label-value-horizontal' | 'label-value-vertical'
       label: string
       iconName?: never
     }
   | {
+      value: string
       variant: 'icon-value'
       label?: never
+      iconName: IconName | OloIconName
+    }
+  | {
+      variant: 'icon-label'
+      label: ReactNode
+      value?: never
       iconName: IconName | OloIconName
     }
 )
@@ -30,14 +42,8 @@ const BasicRowCard = ({ variant, value, label, iconName, className }: BasicRowCa
       <div className="py-3 lg:py-4">
         {variant === 'icon-value' ? (
           <div className="flex gap-3 lg:gap-4">
-            {
-              // TODO This should be extracted to a separate component
-              isBaIcon(iconName) ? (
-                <Icon name={iconName} className="size-5 lg:size-6" />
-              ) : isOloIcon(iconName) ? (
-                <OloIcon name={iconName} className="size-5 lg:size-6" />
-              ) : null
-            }
+            <IconWrapper name={iconName} />
+
             <Typography variant="p-default">{value}</Typography>
           </div>
         ) : variant === 'label-value-horizontal' ? (
@@ -51,6 +57,11 @@ const BasicRowCard = ({ variant, value, label, iconName, className }: BasicRowCa
           <div className="flex flex-col gap-1 lg:gap-2">
             <Typography variant="p-default-black">{label}</Typography>
             <Typography variant="p-default">{value}</Typography>
+          </div>
+        ) : variant === 'icon-label' ? (
+          <div className="flex items-center justify-between">
+            {label}
+            <IconWrapper name={iconName} />
           </div>
         ) : null}
       </div>

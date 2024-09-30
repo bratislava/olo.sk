48b83ada5c2ed4e0b6167e0a5f8527911d39f948
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-09-30 10:41:48 +0200

    #407 Add options to delete collection days by type and to update meilisearch index

--- strapi/src/plugins/waste-collection-days-import/server/routes/index.ts
+++ strapi/src/plugins/waste-collection-days-import/server/routes/index.ts
@@ -7,6 +7,16 @@ export default {
         path: '/update-waste-collection-days',
         handler: 'importXlsxController.updateWasteCollectionDays',
       },
+      {
+        method: 'POST',
+        path: '/delete-waste-collection-days/:type',
+        handler: 'importXlsxController.deleteWasteCollectionDays',
+      },
+      {
+        method: 'POST',
+        path: '/update-meilisearch-waste-collection-days',
+        handler: 'importXlsxController.updateMeilisearchWasteCollectionDays',
+      },
     ],
   },
 }

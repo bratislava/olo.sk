47f8bb1fa56b977b520ecbba53551eebb427cedf
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-10-01 14:27:04 +0200

    #540 Install and use `@strapi/plugin-seo` (#598)
    * #540 Install and use `@strapi/plugin-seo`
    
    * #295 Add sidebar dynamic zone to Pages (#601)

--- next/src/components/page-contents/service/ServicePageContent.tsx
+++ next/src/components/page-contents/service/ServicePageContent.tsx
@@ -15,11 +15,7 @@ type Props = {
 }
 
 const ServicePageContent = ({ service }: Props) => {
-  if (!service.attributes) return null
-
   const { image, sections } = service.attributes ?? {}
-
-  const hasContent = image?.data || sections?.length
 
   return (
     <>
@@ -29,28 +25,26 @@ const ServicePageContent = ({ service }: Props) => {
       </div>
       <SectionContainer className="py-6 md:px-0 lg:py-12">
         <div className="flex flex-col items-start gap-4 md:flex-row lg:gap-8">
-          {hasContent && (
-            <div
-              id={SERVICE_PAGE_ROOT}
-              className="order-2 flex w-full shrink flex-col md:order-1 md:w-[50rem]"
-            >
-              {/* TODO fix y-paddings so we don't change it from here */}
-              <div className="flex flex-col gap-12 [&>*]:py-0 [&>*]:lg:py-0 [&>div>*]:px-0 [&>div>*]:lg:px-0">
-                {image?.data?.attributes?.url ? (
-                  <div className="relative aspect-[800/445] w-full shrink-0 overflow-hidden rounded-lg">
-                    <Image
-                      src={image.data.attributes.url}
-                      alt={image.data.attributes.alternativeText ?? ''}
-                      fill
-                      sizes={generateImageSizes({ default: '100vw' })}
-                      className="object-cover"
-                    />
-                  </div>
-                ) : null}
-                <Sections sections={sections?.filter(isDefined) ?? []} />
-              </div>
+          <div
+            id={SERVICE_PAGE_ROOT}
+            className="order-2 flex w-full shrink flex-col md:order-1 md:w-[50rem]"
+          >
+            {/* TODO fix y-paddings so we don't change it from here */}
+            <div className="flex flex-col gap-12 [&>*]:py-0 [&>*]:lg:py-0 [&>div>*]:px-0 [&>div>*]:lg:px-0">
+              {image?.data?.attributes?.url ? (
+                <div className="relative aspect-[800/445] w-full shrink-0 overflow-hidden rounded-lg">
+                  <Image
+                    src={image.data.attributes.url}
+                    alt={image.data.attributes.alternativeText ?? ''}
+                    fill
+                    sizes={generateImageSizes({ default: '100vw' })}
+                    className="object-cover"
+                  />
+                </div>
+              ) : null}
+              <Sections sections={sections?.filter(isDefined) ?? []} />
             </div>
-          )}
+          </div>
           <div className="order-1 shrink grow max-md:w-full md:max-w-80 lg:order-2">
             {/* Screen Desktop */}
             <div className="hidden lg:block">

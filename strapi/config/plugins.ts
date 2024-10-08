040f9de1d26cee1f72aac2847370b8037e87c7f5
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-10-08 15:59:20 +0200

    Add empty string to region

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -33,4 +33,27 @@ export default ({ env }) => ({
       ],
     },
   },
+  upload: {
+    config: {
+      // Docs: https://github.com/strapi/strapi/tree/main/packages/providers/upload-aws-s3
+      provider: 'aws-s3',
+      providerOptions: {
+        // This is how we set up "subdomain" style of urls, e.g. "bucket-name.s3.bratislava.sk/..."
+        baseUrl: env('MINIO_PUBLIC_ENDPOINT'),
+        // Works like a folder, https://forum.strapi.io/t/how-to-specify-the-folder-for-strapi-provider-upload-aws-s3-plugin/30805/2
+        rootPath: 'upload',
+        s3Options: {
+          credentials: {
+            accessKeyId: env('MINIO_ACCESS_KEY'),
+            secretAccessKey: env('MINIO_SECRET_KEY'),
+          },
+          endpoint: env('MINIO_PRIVATE_ENDPOINT'),
+          region: '',
+          params: {
+            Bucket: env('MINIO_BUCKET'),
+          },
+        },
+      },
+    },
+  },
 })

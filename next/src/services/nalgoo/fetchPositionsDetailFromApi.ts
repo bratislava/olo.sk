3ecd5ba67ab0ef5d3ea96975ed1f20c309a38537
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-10-10 13:41:55 +0200

    Solving eslint issues

--- next/src/services/nalgoo/fetchPositionsDetailFromApi.ts
+++ next/src/services/nalgoo/fetchPositionsDetailFromApi.ts
@@ -2,7 +2,6 @@ import { environment } from '@/src/environment'
 import { JobOfferDetail } from '@/src/services/todo-openapi-nalgoo'
 
 export const fetchPositionsDetailFromApi = async (id: string) => {
-  // more information about "expand" param can be found in the swagger documentation for Nalgoo https://ats.nalgoo.com/api/rest/swagger.json?api_key=
   const fetchUrl = `http://${environment.nextHost}/api/nalgoo-detail/?id=${id}`
 
   const response = await fetch(fetchUrl)

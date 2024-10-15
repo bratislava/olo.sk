0fd93ab2195b54c87f9b24ddac98ddd4ebb016fe
Adam <jakubicka.adam@gmail.com>
2024-10-15 15:32:04 +0200

    Add empty lines for better readability

--- strapi/src/plugins/wysiwyg/admin/src/components/Wysiwyg/index.tsx
+++ strapi/src/plugins/wysiwyg/admin/src/components/Wysiwyg/index.tsx
@@ -74,6 +74,7 @@ const Wysiwyg = ({
             </Typography>
           )}
         </Box>
+
         <Typography variant="pi">
           Poznámka k odkazom: Okrem URL sa dá pridať aj „interný“ odkaz na OLO stránky, články,
           služby, dokumenty alebo workshopy. V tom prípade treba do odkazu namiesto URL napísať
@@ -81,6 +82,7 @@ const Wysiwyg = ({
           "workshop". Namiesto ID treba napísať zodpovedajúce id - je to číslo, ktoré je jedinečné
           pre každú stránku, článok, službu, dokument, či workshop a nájdeme ho cez Správcu obsahu.
         </Typography>
+
         <Button
           startIcon={<Landscape />}
           variant="secondary"
@@ -89,14 +91,18 @@ const Wysiwyg = ({
         >
           Media library
         </Button>
+
         <Editor disabled={disabled} name={name} onChange={onChange} value={value} />
+
         {error && (
           <Typography variant="pi" textColor="danger600">
             {formatMessage({ id: error, defaultMessage: error })}
           </Typography>
         )}
+
         {description && <Typography variant="pi">{formatMessage(description)}</Typography>}
       </Stack>
+
       <MediaLib
         isOpen={mediaLibVisible}
         onChange={handleChangeAssets}

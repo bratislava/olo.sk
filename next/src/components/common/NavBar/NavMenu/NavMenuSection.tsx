1005de0f9ba477ba510f50b687fdb535dfc584cc
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-10-15 12:59:49 +0200

    Ensure that horizontal dividers aren't dependent on the section.hasDividers attribute

--- next/src/components/common/NavBar/NavMenu/NavMenuSection.tsx
+++ next/src/components/common/NavBar/NavMenu/NavMenuSection.tsx
@@ -28,7 +28,7 @@ const NavMenuSection = ({ section, className }: NavMenuSectionProps) => {
       {specialSectionType === 'latest_articles' ? (
         <NavMenuLatestArticlesList hasDividers={hasDividers} />
       ) : multicolumnBehaviour === 'split_equally' && colSpan > 1 && isDesktopBreakpoint ? (
-        <NavMenuTwoColumnList links={links} hasDividers={hasDividers} />
+        <NavMenuTwoColumnList links={links} />
       ) : (
         <NavMenuSingleColumnList links={links} hasDividers={hasDividers} />
       )}
--- next/src/components/common/NavBar/NavMenu/NavMenuTwoColumnList.tsx
+++ next/src/components/common/NavBar/NavMenu/NavMenuTwoColumnList.tsx
@@ -3,21 +3,13 @@ import { NavMenuColumnListProps } from '@/src/components/common/NavBar/NavMenu/N
 import cn from '@/src/utils/cn'
 import { useGetLinkProps } from '@/src/utils/useGetLinkProps'
 
-const NavMenuTwoColumnList = ({ links, hasDividers, className }: NavMenuColumnListProps) => {
+const NavMenuTwoColumnList = ({ links, className }: NavMenuColumnListProps) => {
   const { getLinkProps } = useGetLinkProps()
   const middleIndex = Math.floor(links.length / 2)
 
   return (
-    <ul
-      className={cn(
-        'w-full columns-2 gap-8',
-        { 'divide-y divide-border-default': hasDividers },
-        className,
-      )}
-    >
+    <ul className={cn('w-full columns-2 gap-8', className)}>
       {links.map((link, index) => {
-        const linkProps = getLinkProps(link)
-
         return (
           <div
             key={link.id}
@@ -25,7 +17,10 @@ const NavMenuTwoColumnList = ({ links, hasDividers, className }: NavMenuColumnLi
               'border-r border-border-default pr-8': index <= middleIndex,
             })}
           >
-            <NavMenuLink {...linkProps} className={cn('pb-5', { 'pb-0': index === middleIndex })} />
+            <NavMenuLink
+              {...getLinkProps(link)}
+              className={cn('pb-5', { 'pb-0': index === middleIndex })}
+            />
           </div>
         )
       })}

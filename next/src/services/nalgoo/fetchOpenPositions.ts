120c1c51b35f4dddfed566abba8f82860dbb679f
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-10-17 13:23:46 +0200

    Add headers to Nalgoo endpoints

--- next/src/services/nalgoo/fetchOpenPositions.ts
+++ next/src/services/nalgoo/fetchOpenPositions.ts
@@ -5,9 +5,15 @@
  */
 export const fetchOpenPositions = async () => {
   // TODO: this fetch should not directly fetch the data, but use OpenAPI generation and client with requests
+
   const response = await fetch(
     // eslint-disable-next-line no-secrets/no-secrets
     `${process.env.NEXT_NALGOO_URL}/job-offer?expand=company_sectors,regions,employment_forms,position_type,business_unit,custom_fields&api_key=${process.env.NEXT_NALGOO_API_KEY}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
   )
 
   return response.json()

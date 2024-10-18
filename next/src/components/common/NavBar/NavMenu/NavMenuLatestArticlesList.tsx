d3aba56cae37e920716595628214a03307b96c23
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2024-10-18 08:26:12 +0200

    Revert "Refactor card behaviour in the menu, with respect to links"
    This reverts commit a164c46300eebded8000dd3ad4fafb1b9310d9f6.

--- next/src/components/common/NavBar/NavMenu/NavMenuLatestArticlesList.tsx
+++ next/src/components/common/NavBar/NavMenu/NavMenuLatestArticlesList.tsx
@@ -14,7 +14,6 @@ type NavMenuLatestArticlesListProps = {
   className?: string
 }
 
-// TODO: Fix focus rings for the cards
 const NavMenuLatestArticlesList = ({ hasDividers, className }: NavMenuLatestArticlesListProps) => {
   const { i18n } = useTranslation()
   const locale = i18n.language

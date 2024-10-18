c51561aa353e311f85c6a43e2f270c93bc25cade
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-10-18 12:07:49 +0200

    Set Https

--- next/src/services/nalgoo/fetchOpenPositionsFromApi.ts
+++ next/src/services/nalgoo/fetchOpenPositionsFromApi.ts
@@ -2,7 +2,7 @@ import { environment } from '@/src/environment'
 import { JobOfferListItem } from '@/src/services/todo-openapi-nalgoo'
 
 export const fetchOpenPositionsFromApi = async () => {
-  const fetchUrl = `http://${environment.nextHost}/api/nalgoo`
+  const fetchUrl = `https://${environment.nextHost}/api/nalgoo`
   const response = await fetch(fetchUrl)
 
   return response.status === 200
--- next/src/services/nalgoo/fetchPositionsDetailFromApi.ts
+++ next/src/services/nalgoo/fetchPositionsDetailFromApi.ts
@@ -3,7 +3,7 @@ import { JobOfferDetail } from '@/src/services/todo-openapi-nalgoo'
 
 export const fetchPositionsDetailFromApi = async (id: string) => {
   // more information about "expand" param can be found in the swagger documentation for Nalgoo https://ats.nalgoo.com/api/rest/swagger.json?api_key=
-  const fetchUrl = `http://${environment.nextHost}/api/nalgoo-detail/?id=${id}`
+  const fetchUrl = `https://${environment.nextHost}/api/nalgoo-detail/?id=${id}`
 
   const response = await fetch(fetchUrl)
 

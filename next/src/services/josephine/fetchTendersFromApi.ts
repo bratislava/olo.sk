0d1ace467e98a6c29b99bd50e10667d2ea8a5cf8
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2024-10-22 11:01:12 +0200

    Set relative Josephine URL path

--- next/src/services/josephine/fetchTendersFromApi.ts
+++ next/src/services/josephine/fetchTendersFromApi.ts
@@ -1,11 +1,9 @@
-import { environment } from '@/src/environment'
-
 export const fetchTendersFromApiRunning = async (
   timeframe: string, // more about possible fetch options can be found in Josephine documentation
   currentPage: number,
   tendersPerPage: number,
 ) => {
-  const fetchUrl = `http://${environment.nextHost}/api/josephine?timeframe=${timeframe}&currentPage=${currentPage}&tendersPerPage=${tendersPerPage}`
+  const fetchUrl = `/api/josephine?timeframe=${timeframe}&currentPage=${currentPage}&tendersPerPage=${tendersPerPage}`
 
   const response = await fetch(fetchUrl)
 
